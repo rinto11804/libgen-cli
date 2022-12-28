6b7e5cad598f4456c21e5cb1a5e4c99abf94b344
rinto11804 <rinto11804@gmail.com>
2022-12-28 17:42:33 +0530

    util file made

--- cmd/api.go
+++ cmd/api.go
@@ -1,14 +1,10 @@
 package cmd
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"net/url"
 	"regexp"
-	"time"
 )
 
 const (
@@ -75,32 +71,6 @@ func Search(options *SearchOpt) ([]*Book, error) {
 		return nil, err
 	}
 	return books, nil
-}
-
-func getBody(url string) ([]byte, error) {
-	client := http.Client{
-		Timeout: time.Second * 15,
-		Transport: &http.Transport{
-			Proxy:           http.ProxyFromEnvironment,
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-	res, err := client.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("Cant connect to %s error %s", host, err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("http returned witn status code %s", res.Status)
-	}
-
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-	return b, nil
 }
 
 func hashParser(res []byte) string {
---
+++ cmd/util.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func getBody(url string) ([]byte, error) {
+	client := http.Client{
+		Timeout: time.Second * 15,
+		Transport: &http.Transport{
+			Proxy:           http.ProxyFromEnvironment,
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Cant connect to %s error %s", host, err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http returned witn status code %s", res.Status)
+	}
+
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	return b, nil
+}
